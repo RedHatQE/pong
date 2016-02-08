9b1ddb7c954be2e38512792734631d171e5392a2
Sean Toner <stoner@localhost.localdomain>
2016-02-08 18:22:25 -0500

    Added file logging

--- polarion_testng/logger.py
+++ polarion_testng/logger.py
@@ -1,7 +1,25 @@
-__author__ = 'stoner'
-
 import logging
 import sys
+import time
+
+DEFAULT_LOG_FILE = "/tmp/polarion_testng"
+
+
+def make_timestamp():
+    """
+    Returns the localtime year-month-day-hr-min-sec as a string
+    """
+    timevals = time.localtime()[:-3]
+    ts = "-".join(str(x) for x in timevals)
+    return ts
+
+
+def make_timestamped_filename(prefix, postfix=".log"):
+    """
+    Returns a string containing prefix-timestamp-postfix
+    """
+    fname = prefix + "-" + make_timestamp() + postfix
+    return fname
 
 
 def make_stream_handler(fmt, stream=sys.stdout, loglevel=logging.INFO):
@@ -10,6 +28,20 @@ def make_stream_handler(fmt, stream=sys.stdout, loglevel=logging.INFO):
     strm_handler.setFormatter(fmt)
     strm_handler.setLevel(loglevel)
     return strm_handler
+
+
+def make_file_handler(fmt, filename, loglevel=logging.DEBUG):
+    """
+
+    :param fmt:
+    :param filename:
+    :param loglevel:
+    :return:
+    """
+    file_handler = logging.FileHandler(filename)
+    file_handler.setFormatter(fmt)
+    file_handler.setLevel(loglevel)
+    return file_handler
 
 
 def make_formatter(format_str="", date_format="%H:%M:%S"):
@@ -29,21 +61,29 @@ def make_logger(loggername, handlers=(), loglevel=logging.DEBUG):
     return logr
 
 
-def get_simple_logger(logname, loglvl=logging.DEBUG):
+def get_simple_logger(logname, filename=DEFAULT_LOG_FILE, loglvl=logging.DEBUG):
     """
     Simple wrapper around the other functions to create a basic logger.  This is
     useful as a module level debugger
 
     :param logname: (str) a name to give to the logger object
+    :param filename: (str) path to log file
     :param loglvl: (int) a logging loglevel
     """
     # Do the stream handler and formatter
     stream_fmt = make_formatter()
     sh = make_stream_handler(stream_fmt)
+    sh.setLevel(logging.INFO)
+
+    # Make the filename, file handler and formatter
+    fname = make_timestamped_filename(filename, ".log")
+    file_fmt = make_formatter()
+    fh = make_file_handler(file_fmt, fname)
+    fh.setLevel(logging.DEBUG)
 
     # get the actual logger
-    logr = make_logger(logname, (sh,))
-    logr.setLevel(loglvl)
+    logr = make_logger(logname, (sh, fh))
+    # logr.setLevel(loglvl)
     return logr
 
 log = get_simple_logger(__name__)
