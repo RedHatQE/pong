8301615390f614e0660892c89e26fe8125ad4252
Sean Toner <stoner@localhost.localdomain>
2016-02-08 16:29:32 -0500

    Added COMPOSE_ID to test environment config file, and started writing tests

---
+++ polarion_testng/tests/test_configurators.py
@@ -0,0 +1,16 @@
+import polarion_testng.configuration as cfg
+import pyrsistent as pyr
+
+import unittest
+
+
+class TestCLIConfigurator(unittest.TestCase):
+    def test_args(self):
+        arglist = ["-r https://rhsm-jenkins.rhev-ci-vms.eng.rdu2.redhat.com/view/Scratch/job/stoner-gui-test-smoke/93/artifact/test-output/testng-results.xml",
+                   "--testrun-prefix RHSM",
+                   "--testrun-suffix blah"]
+        argstr = " ".join(arglist)
+        self.cfg = cfg.CLIConfigurator(args=argstr)
+
+        start_map = pyr.m()
+        end_map = self.cfg(start_map)
